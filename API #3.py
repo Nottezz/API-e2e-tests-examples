85174eadf5534dfe2adbacfe5c271638a0945c0a
Nottez <driftworks2013@yandex.ru>
2023-04-11 15:31:40 +0300

    add DELETE request

--- API #3.py
+++ API #3.py
@@ -6,12 +6,14 @@ class New_location():
     def create_new_location(self):
         """Создание локаций"""
 
+        base_url = 'https://rahulshettyacademy.com'  # Базовый URL
+        key = "?key=qaclick123"  # Параметр для всех запросов
+
+        """Метод POST"""
+        print('\nМЕТОД POST\n')
+
         for location in range(5):
 
-            base_url = 'https://rahulshettyacademy.com'  # Базовый URL
-            key = "?key=qaclick123"  # Параметр для всех запросов
-
-            """Метод POST"""
             post_resourece = '/maps/api/place/add/json'  # Ресурст метода POST
 
             post_url = base_url + post_resourece + key
@@ -41,14 +43,16 @@ class New_location():
             check_post = result_post.json()
             place_id = check_post.get('place_id')
             print('Place ID сформирован - ' + place_id)
-            with open('place_id.txt', mode='a') as file:
+            with open('place_id.txt', mode='a') as file: # Запись информации по place_id в файл
                 file.write(place_id + "\n")
 
-        print('\nPlace ID сохранены')
+        print('\nPlace ID сохранены\n')
 
         file = open('place_id.txt')
-        read_file = file.readlines()
+        read_file = file.readlines() # Чтение созданного файла списком
 
+        """Метод GET"""
+        print('МЕТОД GET\n')
         for place_id_list in read_file:
 
             get_resourece = "/maps/api/place/get/json"
@@ -59,7 +63,41 @@ class New_location():
             print(f'Статус код - {result_get.status_code}')
             assert 200 == result_get.status_code
             print('Успешно! Проверка пройдена')
-    print('Проверка New_location пройдена!')
+
+        """Метод DELETE"""
+        print('\nМЕТОД DELETE\n')
+        delete_place = [read_file[1], read_file[3]]
+
+
+        for delete_list in delete_place:
+
+            delete_resource = '/maps/api/place/delete/json'
+            delete_url = base_url + delete_resource + key
+            json_for_delete_new_location = {
+                "place_id": delete_list.rstrip('\n')
+            }
+            result_delete = requests.delete(delete_url, json=json_for_delete_new_location)
+            check_delete = result_delete.json()
+            status = check_delete.get('status')
+            print('Place ID - '+ delete_list.rstrip('\n'))
+            print('Статус удаления локации -' + status)
+
+        """Метод GET"""
+        print('\nМЕТОД GET\n')
+        for place_id_list in read_file:
+
+            get_resourece = "/maps/api/place/get/json"
+            get_url = base_url + get_resourece + key + "&place_id=" + place_id_list.rstrip('\n')
+            print(get_url)
+            result_get = requests.get(get_url)
+            print(f'Статус код - {result_get.status_code}')
+            if result_get.status_code == 200:
+                with open('place_id_new.txt', mode='a') as file:
+                 file.write(place_id_list + "\n")
+
+        print('Place ID обновлены!')
+
+        print('\nПроверка New_location пройдена!')
 
 location = New_location()
 location.create_new_location()
